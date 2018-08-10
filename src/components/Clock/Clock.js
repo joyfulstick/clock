8d3e5d776e038d3dcf3670330944ecf78f537521
Michal Kijewicz <m.kijewicz@raks.pl>
2018-08-10 17:12:52 +0200

    create clock

---
+++ src/components/Clock/Clock.js
@@ -0,0 +1,71 @@
+import './Clock.css'
+import React, { Component } from 'react'
+import Hend from './Hend/Hend'
+
+class Clock extends Component {
+  state = {
+    seconds: {
+      deg: 0,
+      color: 'crimson',
+    },
+    minutes: {
+      deg: 0,
+    },
+    hours: {
+      deg: 0,
+      width: 30,
+      left: 20,
+    },
+  }
+
+  componentDidMount() {
+    this.setTime()
+    setInterval(this.setTime, 1000)
+  }
+
+  setTime = () => {
+    const date = new Date(),
+      tiemZone = date.getTimezoneOffset() / 60
+
+    this.setState(prevState => {
+      return {
+        seconds: {
+          ...prevState.seconds,
+          deg: (((date / 1000) % 60) / 60) * 360,
+        },
+        minutes: {
+          ...prevState.minutes,
+          deg: (((date / (1000 * 60)) % 60) / 60) * 360,
+        },
+        hours: {
+          ...prevState.hours,
+          deg: ((((date / (1000 * 60 * 60)) % 12) - tiemZone) / 12) * 360,
+        },
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  render() {
+    return (
+      <div className="board">
+        <div className="clock">
+          {Object.entries(this.state).map(([k, v]) => (
+            <Hend
+              key={k}
+              deg={v.deg}
+              width={v.width}
+              left={v.left}
+              color={v.color}
+            />
+          ))}
+          <div className="axis" />
+        </div>
+      </div>
+    )
+  }
+}
+export default Clock
---
+++ src/components/Clock/Hend/Hend.js
@@ -0,0 +1,20 @@
+import './Hend.css'
+import React from 'react'
+
+const Hend = props => (
+  <div
+    style={{
+      transform: `rotate(${props.deg}deg)`,
+      width: `${props.width}%`,
+      left: `${props.left}%`,
+      background: props.color,
+      transition:
+        props.deg > 354 || props.deg < 6
+          ? 'transform 0s'
+          : 'transform 0.05s cubic-bezier(0.1, 2.6, 0.6, 1)',
+    }}
+    className="hend"
+  />
+)
+
+export default Hend
